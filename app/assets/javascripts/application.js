06a84d48411b57c4695496e10daa20a64f475ea7
skageraz <skageraz@gmail.com>
2018-08-21 17:58:56 +0200

    tabs colors

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -44,7 +44,7 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 3000); // Change image every 5 seconds
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
 
 
